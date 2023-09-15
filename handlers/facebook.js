6a477e9f2abed441ed235b672d9ba9590a97c3dd
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-09-15 20:30:22 -0300

    edit: folder name

---
+++ handlers/facebook.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+
+const facebookScrapper = () => {
+    
+}
+
+module.exports = facebookScrapper
---
+++ handlers/instagram.js
@@ -0,0 +1,25 @@
+const instaScrapper = (bot, chatId, jsonRespose) => {
+    try {
+        console.log('working')
+        bot.sendMessage(chatId, 'Processing your link, please wait...')
+        const medias = jsonRespose.medias
+        if (medias.length > 0) {
+            medias.forEach(media => {
+                if (media.type === 'video') {
+                    bot.sendVideo(chatId, media.url)
+                } else if (media.type === 'image') {
+                    bot.sendPhoto(chatId, media.url)
+                } else {
+                    bot.sendMessage(chatId, 'There was an error sending your link, please try again.');
+                }
+            })
+        } else {
+            bot.sendMessage('Could not find the media for that link. Please try again');
+        }
+    } catch (error) {
+        console.log(error)
+        bot.sendMessage(chatId, 'An error ocurred while processing the link. Try again!');
+    }
+}
+
+module.exports = instaScrapper
