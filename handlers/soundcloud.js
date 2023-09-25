ff7ee43dc9e7e72de8efe860390974390e3a6a8a
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-09-24 22:12:32 -0300

    feat: added soundcloud support

--- handlers/soundcloud.js
+++ handlers/soundcloud.js
@@ -1,12 +1,24 @@
+const {errHandler, linkError, mediaError} = require('../error/error-handler')
+
 const soundcloudScrapper = async (bot, chatId, medias) => {
     try {
-        
+        await bot.sendMessage(chatId, 'Processing your link, please wait...');
+        console.log(medias)
+        if (medias.length > 0) {
+            if (medias[0].type === 'audio') {
+                bot.sendAudio(chatId, medias[0].url)
+            } else {
+                linkError(bot, chatId)
+            }
+        } else {
+            mediaError(bot, chatId)
+        }
     } catch (error) {
-        
+        errHandler(error, bot, chatId)
     }
 }
 
 module.exports = {
-    linkPrefix: 'https://www.soundcloud.com/',
+    linkPrefix: 'https://soundcloud.com/',
     handle: soundcloudScrapper
 }
