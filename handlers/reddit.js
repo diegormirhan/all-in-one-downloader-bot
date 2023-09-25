fc5e250292a3a1f592bb8a1ef6000d9e06a63f3a
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-09-24 22:12:19 -0300

    feat: added reddit support

--- handlers/reddit.js
+++ handlers/reddit.js
@@ -1,8 +1,17 @@
+const {errHandler, linkError, mediaError} = require('../error/error-handler')
+
 const redditScrapper = async (bot, chatId, medias) => {
     try {
-        
+        await bot.sendMessage(chatId, 'Processing your link, please wait...');
+        sortedMedias = medias.sort((a, b) => (b.format || 0) - (a.format || 0))
+
+        if (medias.length > 0) {
+            bot.sendVideo(chatId, sortedMedias[0].url)
+        } else {
+            mediaError(bot, chatId)
+        }
     } catch (error) {
-        
+        errHandler(error, bot, chatId)
     }
 }
 
