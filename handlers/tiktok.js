0eed1adfe8f89ba9aecbd4d17a4be275ba04bea9
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-09-24 22:12:49 -0300

    feat: added tiktok support

--- handlers/tiktok.js
+++ handlers/tiktok.js
@@ -1,12 +1,19 @@
+const {errHandler, linkError, mediaError} = require('../error/error-handler')
+
 const tiktokScrapper = async (bot, chatId, medias) => {
     try {
-        
+        await bot.sendMessage(chatId, 'Processing your link, please wait...')
+        if (medias.length > 0) {
+            bot.sendVideo(chatId, medias[0].url)
+        } else {
+            mediaError(bot, chatId)
+        }
     } catch (error) {
-        
+        errHandler(error, bot, chatId)
     }
 }
 
 module.exports = {
-    linkPrefix: 'https://tiktok.com/',
+    linkPrefix: 'https://www.tiktok.com/',
     handle: tiktokScrapper
 }
