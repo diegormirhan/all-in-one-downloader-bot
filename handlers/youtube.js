762d5912c85c4ebc19ea38b9808be5a332b6166f
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-09-25 21:16:33 -0300

    add: implementing youtube support

--- handlers/youtube.js
+++ handlers/youtube.js
@@ -2,9 +2,19 @@ const {errHandler, linkError, mediaError} = require('../error/error-handler')
 
 const youtubeScrapper = async (bot, chatId, medias) => {
     try {
-        
+        await bot.sendMessage(chatId, 'Processing your link, please wait...')
+        const sortedMedias = medias.sort((a, b) => (b.bitrate || 0) - (a.bitrate || 0));
+        if (medias.length > 0) {
+            if (sortedMedias[0].extension === 'mp4') {
+                bot.sendVideo(chatId, sortedMedias[0].url)
+            } else {
+                linkError(bot, chatId)
+            }
+        } else {
+            mediaError(bot, chatId)
+        }
     } catch (error) {
-        
+        errHandler(error, bot, chatId)
     }
 }
 
