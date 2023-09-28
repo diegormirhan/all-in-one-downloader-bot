e02141f8856e13197d5108db9d7f22913cce5f7a
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-09-27 22:08:51 -0300

    alt: improved perfomance

---
+++ src/telegram.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const telegramBot = require('node-telegram-bot-api');
+
+// Telegram API
+const token = process.env.TELEGRAM_API
+
+// Declaring the bot
+const bot = new telegramBot(token, {polling: true});
+
+module.exports = {
+    bot
+}
