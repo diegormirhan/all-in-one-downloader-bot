e77f5ea7f09f34535028a8004568c5ad1516f38b
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-09-27 22:09:56 -0300

    alt: changed folder

---
+++ bot/bot-commands.js
@@ -0,0 +1,28 @@
+const { bot } = require('../src/telegram')
+const runMongoDb = require('../bot/get-userId')
+const { userUpdates } = require('./user-updates')
+
+// Create /start command
+bot.onText(/\/start/, function(msg) {
+    const chatId = msg.chat.id
+    const message = "Welcome to the *All In One Bot Downloader*\nYou can download media from your favorite social media.\nFor help, just type /help in the chat."
+    bot.sendMessage(chatId, message, {parse_mode: 'Markdown'});
+    runMongoDb(chatId);
+})
+
+// Create /help command
+bot.onText(/\/help/, function(msg) {
+    const chatId = msg.chat.id
+    const message = "You can download media from different sources, such as:\n*Instagram*\n*Twitter*\n*Pinterest*\n*TikTok*\n*YouTube*\n*Reddit*\n*Facebook*\n*SoundCloud*\nand many others...\n\n*Download Guide:* Just send the entire media link in the chat and wait for the bot response.\nEnjoy your bot :)"
+    bot.sendMessage(chatId, message, {parse_mode: 'Markdown'});
+})
+
+bot.onText(/\/update/, function(msg) {
+    const chatId = msg.chat.id
+    adminId = process.env.ADMIN_ID
+    if (chatId == adminId) {
+        userUpdates(bot, chatId)
+    } else {
+        bot.sendMessage(chatId, 'Você não tem permissão para usar este comando!.')
+    }
+})
