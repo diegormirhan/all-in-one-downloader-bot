06d8791f65a968151cd67a70e4763e589715dcb2
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-09-27 22:09:07 -0300

    alt: improved perfomance

--- database/mongodb.js
+++ database/mongodb.js
@@ -2,29 +2,12 @@ require('dotenv').config()
 const MongoClient = require('mongodb').MongoClient;
 
 const uri = process.env.DATABASE_URL
-
 const client = new MongoClient(uri);
+const idDatabase = client.db('chatId');
+const idCollection = idDatabase.collection('Chat Id Collection')
 
-async function runMongoDb(chatId) {
-    try {
-        const database = client.db('chatId');
-        const coll = database.collection('Chat Id Collection')
-
-        const doc = {
-            id: chatId
-        }
-
-        const existingDoc = await coll.findOne({id: chatId});
-
-        if (!existingDoc) {
-            const result = await coll.insertOne(doc)
-            console.log(`A document was inserted with the _id: ${result.insertedId}`)
-        } else {
-            console.log('The chatID is already on the database')
-        }
-    }  catch(error) {
-        console.log('There was an erro trying to insert the chat Id into the database.\nError Message:', error);
-    }
+module.exports = {
+    client,
+    idDatabase,
+    idCollection
 }
-
-module.exports = runMongoDb
