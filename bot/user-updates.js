74c2012843bca836200eb98742e21c8fa872b644
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-10-01 17:35:40 -0300

    alt: doc finished

--- bot/user-updates.js
+++ bot/user-updates.js
@@ -1,6 +1,13 @@
 require('dotenv').config()
 const { idCollection } = require('../database/mongodb')
 
+/**
+ * Executes a series of tasks to update user information.
+ *
+ * @param {Object} bot - The bot object used to interact with the chat.
+ * @param {number} chatId - The ID of the chat where the updates will be sent.
+ * @return {Promise} A promise that resolves when the updates are complete.
+ */
 const userUpdates = async (bot, chatId) => {
     const documents = await idCollection.find({}).toArray();
     const reply_markup = {
@@ -13,20 +20,20 @@ const userUpdates = async (bot, chatId) => {
         const adminPassword = process.env.ADMIN_PASSWORD
 
         if (adminPassword === msg.text && msg.reply_to_message) {
-            bot.sendMessage(chatId, '*Admin autorizado com sucesso!*\nDigite a mensagem de atualização abaixo que deseja enviar para os usuários.', { parse_mode: 'Markdown', reply_markup: reply_markup })
+            bot.sendMessage(chatId, '*Admin sucess authorized!*\nType the message you want to send to the users below:', { parse_mode: 'Markdown', reply_markup: reply_markup })
             bot.once('message', (msg) => {
                 if (msg.reply_to_message) {
                     documents.forEach((doc) => {
                         bot.sendMessage(doc.id, msg.text, { parse_mode: 'Markdown' })
                     })
                 } else {
-                    bot.sendMessage(chatId, '*Operação Cancelada*', { parse_mode: 'Markdown' })
+                    bot.sendMessage(chatId, '*Operation denied!*', { parse_mode: 'Markdown' })
                 }
             })
         } else if (!msg.reply_to_message) {
-            bot.sendMessage(chatId, '*Operação Cancelada*', { parse_mode: 'Markdown' })
+            bot.sendMessage(chatId, '*Operation denied!*', { parse_mode: 'Markdown' })
         } else {
-            bot.sendMessage(chatId, '*Senha incorreta!*', { parse_mode: 'Markdown' })
+            bot.sendMessage(chatId, '*Wrong password!*', { parse_mode: 'Markdown' })
         }
     })
 }
