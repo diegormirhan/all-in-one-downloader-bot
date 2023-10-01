019fda53f1ecfae2b008c1695cb6410ed48af876
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-10-01 17:35:59 -0300

    alt: doc finished

--- database/mongodb.js
+++ database/mongodb.js
@@ -1,9 +1,13 @@
 require('dotenv').config()
 const MongoClient = require('mongodb').MongoClient;
 
+// Getting the database URL
 const uri = process.env.DATABASE_URL
+// Connecting to the MongoDB
 const client = new MongoClient(uri);
+// Connecting to the chatId database
 const idDatabase = client.db('chatId');
+// Connecting to the Chat Id Collection
 const idCollection = idDatabase.collection('Chat Id Collection')
 
 module.exports = {
