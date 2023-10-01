0a205ced335823b4bf4108cb46f802deac9ee9c0
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-10-01 17:36:10 -0300

    alt: doc finished

--- handlers/facebook.js
+++ handlers/facebook.js
@@ -1,5 +1,13 @@
 const {errHandler, linkError, mediaError} = require('../error/error-handler')
 
+/**
+ * Scrapes a Facebook media from a given link and sends it as a video to the specified chat.
+ *
+ * @param {object} bot - The bot object used to send messages and videos.
+ * @param {number} chatId - The ID of the chat to send the video to.
+ * @param {array} medias - An array of media objects containing the URL of the video.
+ * @return {Promise} A promise that resolves when the video is sent successfully, or rejects with an error.
+ */
 const faceScrapper = async (bot, chatId, medias) => {
     try {
         await bot.sendMessage(chatId, 'Processing your link, please wait...')
