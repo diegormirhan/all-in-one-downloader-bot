9f25b30d883ce3d073f4104bde25a61d71a5238b
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-10-01 17:36:17 -0300

    alt: doc finished

--- handlers/youtube.js
+++ handlers/youtube.js
@@ -1,5 +1,13 @@
 const {errHandler, linkError, mediaError} = require('../error/error-handler')
 
+/**
+ * Scrapes YouTube media from the given bot and chat ID.
+ *
+ * @param {Object} bot - The bot object used to send messages.
+ * @param {number} chatId - The ID of the chat where the message will be sent.
+ * @param {Array} medias - An array of media objects to process.
+ * @return {Promise} A Promise that resolves when the scraping is complete.
+ */
 const youtubeScrapper = async (bot, chatId, medias) => {
     try {
         await bot.sendMessage(chatId, 'Processing your link, please wait...')
