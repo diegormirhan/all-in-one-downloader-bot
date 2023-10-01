5cdad36fada37b65dff3df7e64f01a35a68c8514
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-10-01 17:36:03 -0300

    alt: doc finished

--- handlers/tiktok.js
+++ handlers/tiktok.js
@@ -1,5 +1,13 @@
 const {errHandler, linkError, mediaError} = require('../error/error-handler')
 
+/**
+ * Sends a tiktok video to a specified chat ID using the provided bot object and returns the media file to the user.
+ *
+ * @param {Object} bot - The bot object used to send the video.
+ * @param {number} chatId - The ID of the chat to send the video to.
+ * @param {Array} medias - An array of media objects.
+ * @return {Promise} A promise that resolves when the video is sent successfully.
+ */
 const tiktokScrapper = async (bot, chatId, medias) => {
     try {
         await bot.sendMessage(chatId, 'Processing your link, please wait...')
