d9b4e2f9d29b38ce547effdfaf32889b5005a396
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-10-01 17:36:07 -0300

    alt: doc finished

--- handlers/reddit.js
+++ handlers/reddit.js
@@ -1,5 +1,13 @@
 const {errHandler, linkError, mediaError} = require('../error/error-handler')
 
+/**
+ * Asynchronously scrapes a Reddit page and sends the highest-rated video to a specified chat.
+ *
+ * @param {object} bot - The Telegram bot instance.
+ * @param {number} chatId - The ID of the chat to send the video to.
+ * @param {array} medias - An array of media objects to process and send.
+ * @return {undefined} - This function does not return anything.
+ */
 const redditScrapper = async (bot, chatId, medias) => {
     try {
         await bot.sendMessage(chatId, 'Processing your link, please wait...');
