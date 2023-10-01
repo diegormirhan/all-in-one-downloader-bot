9b3a2ab983dfd7aa3899bb0de1ae3b08a6467ccc
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-10-01 17:35:47 -0300

    alt: doc finished

--- handlers/soundcloud.js
+++ handlers/soundcloud.js
@@ -1,5 +1,13 @@
 const { errHandler, linkError, mediaError } = require('../error/error-handler')
 
+/**
+ * Scrapes SoundCloud media and sends it to the specified chat.
+ *
+ * @param {Object} bot - The bot object used to send messages.
+ * @param {number} chatId - The ID of the chat where the media will be sent.
+ * @param {Array} medias - The array of media objects to be processed.
+ * @return {Promise} A promise that resolves when the media is sent successfully.
+ */
 const soundcloudScrapper = async (bot, chatId, medias) => {
     try {
         await bot.sendMessage(chatId, 'Processing your link, please wait...');
