cd08a52878c4a663e20db06a61636d1be5ec3752
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-10-01 17:35:55 -0300

    alt: doc finished

--- handlers/pinterest.js
+++ handlers/pinterest.js
@@ -1,5 +1,13 @@
 const {errHandler, linkError, mediaError} = require('../error/error-handler')
 
+/**
+ * Scrapes the pinterest link and sends the corresponding media to a chat.
+ *
+ * @param {Object} bot - The bot object used for sending messages.
+ * @param {number} chatId - The ID of the chat to send the media to.
+ * @param {Array} medias - An array of media objects to scrape.
+ * @return {Promise} A promise that resolves when the media is sent.
+ */
 const pinScrapper = async (bot, chatId, medias) => {
     const media = medias.reverse()[0]
     try {
