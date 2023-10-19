b679f7963d1709e8693b6933a67f6cad8a5270a5
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-10-19 09:38:56 -0300

    fixed known bugs

--- bot/get-media.js
+++ bot/get-media.js
@@ -12,11 +12,11 @@ console.log(modules)
 // Listening to the user message and return media file from social media
 bot.on('message', async function(msg) {
     const chatId = msg.chat.id
-    const medias = await scrapper(msg.text)
+    const medias = scrapper(msg.text)
     for (const handlerName in modules) {
-    if (msg.text.startsWith(modules[handlerName].linkPrefix)) {
-        await modules[handlerName].handle(bot, chatId, medias)
-        break
-    }
-    }
+        if (msg.text.startsWith(modules[handlerName].linkPrefix)) {
+            await modules[handlerName].handle(bot, chatId, medias)
+            break
+        }
+        }
 })
