27390c3b6dc35b99a694f359f2eda1041a5160af
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-10-21 18:30:22 -0300

    alt: removed fastify

--- app.js
+++ app.js
@@ -3,17 +3,5 @@ const bot = require('./src/telegram');
 const getMedia = require('./bot/get-media')
 const botCommands = require('./bot/bot-commands')
 const getUserID = require('./bot/get-userId')
-const fastify = require('fastify')({
-    logger: false
-})
 
-
-// Declaring fastify 3000 port route
-fastify.listen({port: process.env.PORT || 3000}, function (err, adress) {
-    if (err) {
-        fastify.log.error(err);
-        process.exit(1);
-    }
-});
-
-
+console.log('-- Bot is alive! --')
