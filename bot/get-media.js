0e4046cdb577704db523bfb3c3456b4f37dc13d6
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-10-21 18:29:03 -0300

    alt: fixed some bot issues

--- bot/get-media.js
+++ bot/get-media.js
@@ -5,18 +5,26 @@ const scrapper = require('../api/scrapper');
 
 // Import handlers
 const folderPath = path.join(__dirname, '../handlers');
-const modules = requireAll({dirname: folderPath});
-console.log(modules)
+const modules = requireAll({ dirname: folderPath });
 
 
 // Listening to the user message and return media file from social media
-bot.on('message', async function(msg) {
+bot.on('message', async function (msg) {
     const chatId = msg.chat.id
-    const medias = scrapper(msg.text)
-    for (const handlerName in modules) {
-        if (msg.text.startsWith(modules[handlerName].linkPrefix)) {
-            await modules[handlerName].handle(bot, chatId, medias)
-            break
+    try {
+        const medias = await scrapper(msg.text)
+        if (typeof (msg.text) === 'string') {
+            for (const handlerName in modules) {
+                if (msg.text.startsWith(modules[handlerName].linkPrefix)) {
+                    await modules[handlerName].handle(bot, chatId, medias)
+                    break
+                }
+            }
+        } else {
+            bot.sendMessage(chatId, '*Please send a valid message containing the link*', { parse_mode: 'Markdown' })
+            console.log('Not a valid message sent - Err 404')
         }
-        }
+    } catch (error) {
+        console.error('Error handling message', error)
+    }
 })
