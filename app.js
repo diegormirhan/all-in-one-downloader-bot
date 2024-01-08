bdc5e418ba010989d272249c6f86e75c5462ccf4
Vivek Tp <Vivektvp1@gmail.com>
2024-01-08 06:26:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,4 +4,16 @@ const getMedia = require('./bot/get-media')
 const botCommands = require('./bot/bot-commands')
 const getUserID = require('./bot/get-userId')
 
+const express = require('express')
+const path = require('path')
+
+const PORT = process.env.PORT || 5001
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
 console.log('-- Bot is alive! --')
