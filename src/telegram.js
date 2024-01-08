77e919e3880ed86fb4fb8dcb70c3e5f5f6ca5b3d
Vivek Tp <Vivektvp1@gmail.com>
2024-01-08 12:48:38 +0530

    Update telegram.js

--- src/telegram.js
+++ src/telegram.js
@@ -1,6 +1,11 @@
 require('dotenv').config()
 const telegramBot = require('node-telegram-bot-api');
 
+const port = process.env.PORT || 3000; // Use the provided port or a default one (e.g., 3000)
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
 // Telegram API
 const token = process.env.TELEGRAM_API
 
