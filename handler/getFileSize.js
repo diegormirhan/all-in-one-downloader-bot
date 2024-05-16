6b7449e755552d603e6509729cbf9c6ff64c70df
Diego Mirhan <mirhan.diego@gmail.com>
2024-05-16 17:02:59 -0300

    Check if file size is greater than 20MB

---
+++ handler/getFileSize.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+
+const getFileSize = async (url) => {
+    try {
+        const response = await axios.head(url);
+        const contentLength = response.headers['content-length'];
+        if (contentLength) {
+            return parseInt(contentLength, 10);
+        } else {
+            console.log('Error trying to get content length');
+        }
+    } catch (error) {
+        console.error('Erro ao buscar o tamanho do arquivo', error);
+    }
+}
+
+module.exports = { getFileSize }
