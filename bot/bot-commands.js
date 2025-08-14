d62432f6577529beec114f1e64c8691adf33c696
diegormirhan <mirhan.diego@gmail.com>
2025-08-14 16:29:17 -0300

    .

--- bot/bot-commands.js
+++ bot/bot-commands.js
@@ -13,7 +13,7 @@ bot.onText(/\/donate/, donateCommand)
 // Create the command functions
 async function startCommand(msg) {
     const chatId = msg.chat.id
-    const message = "Welcome to the *All In One Bot Downloader*\nYou can download media from your favorite social media.\nFor help, just type /help in the chat. \n\n*Leaked Porn Here* 👉 @vipvazaadosbot"
+    const message = "Welcome to the *All In One Bot Downloader*\nYou can download media from your favorite social media.\nFor help, just type /help in the chat. \n\n*Leaked Porn Here* 👉 @vipchannel4kbot"
     await bot.sendMessage(chatId, message, { parse_mode: 'Markdown' });
     runMongoDb(msg, chatId);
 }
@@ -40,7 +40,7 @@ bot.on("message", async function (msg) {
                 cleanLink = inputText
                 
                 const downloadUrl = `https://bestmediatool.com?utm_source=telegram&link=${encodeURIComponent(cleanLink)}`;
-                const message = "After clicking the download button, you will be redirected to another page to start downloading the media!\n\n*🔥 Leaked Porn Here 👉 @vipvazaadosbot*";
+                const message = "After clicking the download button, you will be redirected to another page to start downloading the media!\n\n*🔥 Leaked Porn Here 👉 @vipchannel4kbot*";
 
                 bot.sendMessage(chatId, message, {
                     reply_markup: {
